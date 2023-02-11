43ebcd512b14d9639a0af03ac946e78ae0d726ee
apurvasurani96 <artandapurva@gmail.com>
2023-02-11 15:47:27 +0530

    MVC assignment1

---
+++ Assignment_MVC/Assignment_MVC/Controllers/AssignmentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_MVC;
+using System.Data.Entity;
+using Assignment_MVC.Models;
+
+namespace Assignment_MVC.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        NorthwindEntities tb = new NorthwindEntities();
+        // GET: Assignment
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult GetData()
+        {
+
+            //tb.Cust_Data().ToList();
+            
+            return View(tb.Cust_Data().ToList());
+        }
+        public ActionResult GetOrderData()
+        {
+            // tb.ord_data().ToList(); 
+            //tb.SaveChanges();
+            return View("GetOrderData", tb.ord_data().Where(x => x.OrderID == 10248).ToList());   
+        }
+    }
+}
---
+++ Assignment_MVC/Assignment_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment_MVC.MvcApplication" Language="C#" %>
